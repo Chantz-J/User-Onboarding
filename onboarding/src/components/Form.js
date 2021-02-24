b594d4eccb515264d7ec9b87078b7ad7a049e2de
Chantz-J <johnson.chantz@gmail.com>
2021-02-24 17:55:18 -0500

    First commit

---
+++ onboarding/src/components/Form.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect} from 'react'
+
+export default function Form(){
+    return (
+        <section className="form-section">
+            <form>
+                <label>
+                    <input />
+                </label>
+                <label>
+                    <input />
+                </label>
+                <label>
+                    <input />
+                </label>
+                <label>
+                    <input />
+                </label>
+            </form>
+        </section>
+    )
+}
